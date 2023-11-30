ad246ff7f39688aba8b81b7d01f23d24575e7922
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-11-30 16:52:58 -0300

    feat: add express adapter

---
+++ src/infra/http/ExpressAdapter.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+
+import { HttpServer } from './HttpServer';
+
+export class ExpressAdapter implements HttpServer {
+  app: express.Express;
+
+  constructor() {
+    this.app = express();
+  }
+
+  on(method: string, url: string, callback: Function): void {
+    this.app[method as keyof express.Application](
+      url,
+      async (request: express.Request, response: express.Response) => {
+        const output = await callback(
+          request.params,
+          request.body,
+          request.headers
+        );
+        response.json(output);
+      }
+    );
+  }
+
+  listen(port: number): void {
+    this.app.listen(port);
+  }
+}
