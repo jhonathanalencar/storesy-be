a6e46e6b02ae6b973161211bc56f2c7d0be5a853
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-02-21 18:00:04 -0300

    feat: update category controller

--- src/infra/controller/CategoryControllerHttp.ts
+++ src/infra/controller/CategoryControllerHttp.ts
@@ -2,16 +2,28 @@ import type { Request, Response } from 'express';
 
 import { CategoryController } from '../../application/controller/CategoryController';
 import { ListAllCategories } from '../../application/usecase/category/ListAllCategories';
+import { CreateCategory } from '../../application/usecase/category/CreateCategory';
+import { GetCategory } from '../../application/usecase/category/GetCategory';
+import { UpdateCategory } from '../../application/usecase/category/UpdateCategory';
 
 export class CategoryControllerHttp implements CategoryController {
-  constructor(private readonly listAllCategories: ListAllCategories) {}
+  constructor(
+    private readonly createCategory: CreateCategory,
+    private readonly getCategory: GetCategory,
+    private readonly listAllCategories: ListAllCategories,
+    private readonly updateCategory: UpdateCategory
+  ) {}
 
-  create(request: Request, response: Response): Promise<void> {
-    throw new Error('Method not implemented.');
+  async create(request: Request, response: Response): Promise<void> {
+    const body = request.body;
+    const output = await this.createCategory.execute(body);
+    response.status(201).json(output);
   }
 
-  getById(request: Request, response: Response): Promise<void> {
-    throw new Error('Method not implemented.');
+  async getById(request: Request, response: Response): Promise<void> {
+    const { categoryId } = request.params;
+    const output = await this.getCategory.execute({ categoryId });
+    response.status(200).json(output);
   }
 
   async list(request: Request, response: Response): Promise<void> {
@@ -19,7 +31,10 @@ export class CategoryControllerHttp implements CategoryController {
     response.status(200).json(categories);
   }
 
-  update(request: Request, response: Response): Promise<void> {
-    throw new Error('Method not implemented.');
+  async update(request: Request, response: Response): Promise<void> {
+    const { categoryId } = request.params;
+    const { name } = request.body;
+    await this.updateCategory.execute({ categoryId, name });
+    response.status(204).send();
   }
 }
