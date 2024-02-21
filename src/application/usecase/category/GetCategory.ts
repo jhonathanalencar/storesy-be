1a08a049b64364ba9ad6c6d82db9ad9e09a97c87
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-02-21 17:47:44 -0300

    feat: add GetCategory use case

---
+++ src/application/usecase/category/GetCategory.ts
@@ -0,0 +1,24 @@
+import { NotFoundError } from '../../errors/NotFound';
+import { CategoryRepository } from '../../repository/CategoryRepository';
+
+export class GetCategory {
+  constructor(private readonly categoryRepository: CategoryRepository) {}
+
+  async execute(input: Input): Promise<Output> {
+    const category = await this.categoryRepository.getById(input.categoryId);
+    if (!category) throw new NotFoundError('Category not found');
+    return {
+      categoryId: category.name,
+      name: category.name,
+    };
+  }
+}
+
+export type Input = {
+  categoryId: string;
+};
+
+export type Output = {
+  categoryId: string;
+  name: string;
+};
